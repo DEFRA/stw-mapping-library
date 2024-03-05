8c342059a4a1565bbed673331d03516b990505fc
Reece Bennett <reeceb@kainos.com>
2024-03-05 14:10:14 +0000

    Update test outputs

--- trade-mapping-to-ipaffs/src/test/java/uk/gov/defra/stw/mapping/toipaffs/integration/ChedppNotificationMapperIntegrationTest.java
+++ trade-mapping-to-ipaffs/src/test/java/uk/gov/defra/stw/mapping/toipaffs/integration/ChedppNotificationMapperIntegrationTest.java
@@ -2,8 +2,10 @@
 package uk.gov.defra.stw.mapping.toipaffs.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.defra.stw.mapping.toipaffs.testutils.ResourceUtils.readFileToString;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.defra.stw.mapping.dto.SpsCertificate;
 import uk.gov.defra.stw.mapping.toipaffs.chedpp.ChedppNotificationMapper;
-import uk.gov.defra.stw.mapping.toipaffs.testutils.JsonDeserializer;
-import uk.gov.defra.stw.mapping.toipaffs.testutils.ResourceUtils;
 import uk.gov.defra.stw.mapping.toipaffs.testutils.TestUtils;
 import uk.gov.defra.tracesx.notificationschema.representation.Notification;
 
@@ -30,31 +30,26 @@ class ChedppNotificationMapperIntegrationTest {
   @Test
   void map_ReturnsChedppNotification_WhenCompleteEphytoSpsCertificate() throws Exception {
     ObjectMapper objectMapper = TestUtils.initObjectMapper();
+    SpsCertificate spsCertificate = objectMapper.readValue(
+        readFileToString("classpath:chedpp/chedpp_ehc_complete.json"), SpsCertificate.class);
 
-    SpsCertificate spsCertificate =
-        JsonDeserializer.get(
-            SpsCertificate.class, "chedpp/chedpp_ehc_complete.json", objectMapper);
+    Notification actual = chedppNotificationMapper.map(spsCertificate);
+    overrideUniqueComplementIdToStaticValue(actual);
 
-    String expectedNotification =
-        ResourceUtils.readFileToString(
-            "classpath:chedpp/chedpp_ipaffs_integration_complete.json");
-
-    Notification notification = chedppNotificationMapper.map(spsCertificate);
-    String actualNotification = objectMapper.writeValueAsString(notification);
-
-    assertThat(actualNotification)
-        .isEqualTo(replaceUniqueComplementId(notification, expectedNotification));
+    Notification expected = objectMapper.readValue(
+        readFileToString("classpath:chedpp/chedpp_ipaffs_complete.json"), Notification.class);
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .withStrictTypeChecking()
+        .isEqualTo(expected);
   }
 
-  private String replaceUniqueComplementId(Notification notification, String expectedNotification) {
-    return expectedNotification.replaceAll(
-        "REPLACE_ME",
-        notification
-            .getPartOne()
-            .getCommodities()
-            .getComplementParameterSet()
-            .get(0)
-            .getUniqueComplementID()
-            .toString());
+  private void overrideUniqueComplementIdToStaticValue(Notification notification) {
+    UUID staticComplementId = UUID.fromString("00000000-0000-0000-0000-000000000000");
+    notification.getPartOne()
+        .getCommodities()
+        .getComplementParameterSet()
+        .forEach(complementParameterSet ->
+            complementParameterSet.setUniqueComplementID(staticComplementId));
   }
 }
